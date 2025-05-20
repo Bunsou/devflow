fc52857f63aeda8d4b25fc02bafb2a19b789013f
Taing Bunsou <taing.bunsou24@kit.edu.kh>
2025-05-20 10:05:26 +0700

    Set up Light & Dark Theme, Shadcn, Navbar.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 const Home = () => {
   return (
-    <div>
+    <>
       <h1 className="h1-bold">Welcome to Next.js</h1>
-      <h1 className="h1-bold font-space-grotesk">Welcome</h1>
-    </div>
+    </>
   );
 };
 

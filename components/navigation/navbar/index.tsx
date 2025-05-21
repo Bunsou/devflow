7c8989e845828f6720b20eae3cf0676238a9c3f9
Taing Bunsou <taing.bunsou24@kit.edu.kh>
2025-05-21 20:58:02 +0700

    MobileNav and NavLinks

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,18 +2,21 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import ROUTES from "@/constants/routes";
+
+import MobileNavigation from "./MobileNavigation";
 import Theme from "./Theme";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 shadow-light-300 gap-5">
-        <Link href="/" className="flex items-center gap-1">
+      <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5  p-6 shadow-light-300 dark:shadow-none sm:px-12">
+        <Link href={ROUTES.HOME} className="flex items-center gap-1">
           <Image
             src="/images/site-logo.svg"
-            alt="DevFlow Logo"
             width={23}
             height={23}
+            alt="Dev Overflow Logo"
           />
 
           <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
@@ -25,6 +28,8 @@ const Navbar = () => {
 
         <div className="flex-between gap-5">
           <Theme />
+
+          <MobileNavigation />
         </div>
       </nav>
     </>
